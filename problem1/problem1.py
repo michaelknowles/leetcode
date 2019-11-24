e25febd7514a937ed109e29a70260ba8659dfc96
Mike Knowles <mikewknowles@outlook.com>
2019-11-23 17:13:21 -0800

    Problem 1: Python second solution

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -1,5 +1,7 @@
 from typing import List
 
+# This first solution is the naive, brute-force approach
+# It has the least amount of space used (O(1)) but takes the longest (O(n^2))
 def twoSum(nums: List[int], target: int) -> List[int]:
     # Loop through each number in list
     for idx, num in enumerate(nums):
@@ -7,7 +9,25 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         # Start the second loop after the index of the given number
         # to avoid duplicate work
         for jdx, comp in enumerate(nums[idx+1:]):
+            # The list may not be sorted so we cannot short-circuit
             if target == comp + num:
                 return [idx, jdx + idx + 1]
 
 print(twoSum([2, 7, 11, 15], 9) == [0, 1])
+print(twoSum([2, 11, 15, 7], 9) == [0, 3])
+
+# This solution should be the fastest possible (O(n)) since it only loops once
+# It does require building a dictionary of up to n elements (O(n))
+def twoSum2(nums: List[int], target: int) -> List[int]:
+    # Create a dict of complements and indexes as we loop through
+    complements = {}
+    for idx, num in enumerate(nums):
+        comp = target - num
+        # If we've seen the complement, return the indexes
+        if comp in complements:
+            return [complements[comp], idx]
+        else:
+            complements[num] = idx
+
+print(twoSum2([2, 7, 11, 15], 9) == [0, 1])
+print(twoSum2([2, 11, 15, 7], 9) == [0, 3])
