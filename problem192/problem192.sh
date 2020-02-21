1d317a4912a0ff5e9b749a6deef3ab937c086296
Mike Knowles <mikewknowles@outlook.com>
2020-02-20 20:42:47 -0800

    Problem 192

---
+++ problem192/problem192.sh
@@ -0,0 +1,4 @@
+# Read from the file words.txt and output the word frequency list to stdout.
+tr -s ' ' '\n' < words.txt | sort | uniq -c | sort -r | awk '{print $2,$1}'
+
+# a little awkward because it needs the answer in a specific format
